f3eb680aaa9abaf5d83ef95f3def66a95a17c81e
박수아 <xlalfskdl@naver.com>
2021-03-22 23:58:48 +0900

    [ADD] frontend/skeleton code and example typescirpt code
    Former-commit-id: 962bf77b68a397022002b64452ee8cfe9dcb0a3e [formerly dc836b41fd41fcd67d09be5283413b01dd292b6d]
    Former-commit-id: 042279688aa7ab096c73e14b89d6c56fbe635e66

---
+++ frontend/src/store/types.ts
@@ -0,0 +1,35 @@
+import { CommitOptions, DispatchOptions, Store } from "vuex";
+import { Getters } from "./getters";
+import { Actions } from "./actions";
+import { Mutations, MutationTypes } from "./mutations";
+import { RootState } from "./state";
+
+type MyMutations = {
+  commit<K extends keyof Mutations, P extends Parameters<Mutations[K]>[1]>(
+    key: K,
+    payload?: P,
+    options?: CommitOptions
+  ): ReturnType<Mutations[K]>;
+};
+type MyActions = {
+  dispatch<K extends keyof Actions>(
+    key: K,
+    payload?: Parameters<Actions[K]>[1],
+    options?: DispatchOptions
+  ): ReturnType<Actions[K]>;
+};
+
+// type A = keyof Getters;
+type MyGetters = {
+  getters: {
+    [K in keyof Getters]: ReturnType<Getters[K]>;
+  };
+};
+
+export type MyStore = Omit<
+  Store<RootState>,
+  "commit" | "dispatch" | "getters"
+> &
+  MyMutations &
+  MyActions &
+  MyGetters;
