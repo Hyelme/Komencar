72770ddf4fc93f908cf155c5743e97c2f00e3982
박수아 <xlalfskdl@naver.com>
2021-03-23 02:01:57 +0900

    [ADD] feat:frontend/ get googleUser info
    Former-commit-id: ad36aec85c6b85400dafbed6f12e17cb06131fab [formerly 62d69b4b13f17d9979efae6799dbd7e938d2b4c4]
    Former-commit-id: 139e3f92819003ba85a4074507ed1f8530e68844

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="app">
-    
+    <LoginPage/>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import LoginPage from './views/user/LoginPage.vue';
 
 export default Vue.extend({
+  components: { LoginPage },
   name: "App",
   
 });
---
+++ frontend/src/views/user/LoginPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <GoogleLogin/>
+  </div>
+</template>
+
+<script lang="ts">
+import GoogleLogin from '@/components/user/GoogleLogin.vue'
+import Vue from 'vue'
+export default Vue.extend({
+  components: { GoogleLogin },
+})
+</script>
+
+<style>
+
+</style>
