664afe661c681c3339ec0b9afa614ccbd6305c07
배상웅 <dentist312@naver.com>
2021-03-23 13:36:57 +0900

    [UPDATE]restore git error
    Former-commit-id: b3a1fc7902c5833166b20edd749613f24f26c0f2 [formerly b2d68d5252dd99880edbd07771559d950b1dabb1] [formerly 6bd6e069e96a23178e18f43241095b4c157038ef [formerly 2e7096f854199e3e6798a46284a156dc295327e1]]
    Former-commit-id: 1d2b3821c24bc9db41f2335f03c5c2a90a597aa7 [formerly bcc1c170ec7625c8f68c68327e5ffb5587e53f57]
    Former-commit-id: 8f3158c09145423be7b21c903c23ba56f6db873a

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,22 +1,16 @@
 import Vue from "vue";
 import App from "./App.vue";
-<<<<<<< HEAD
 import router from "./routes/index";
 import store from "./store/index";
 // import '@/css/index.css'
 import GAuth from 'vue-google-oauth2'
 
 Vue.use(GAuth, {clientId: '239626612902-num4kon1amg6h3bkgart12e1q18hqprq.apps.googleusercontent.com', scope: 'profile email https://www.googleapis.com/auth/plus.login'})
-=======
->>>>>>> 195dafdae9287fdcc86cea3f94a2cb102b744cd8
 
 Vue.config.productionTip = false;
 
 new Vue({
-<<<<<<< HEAD
   router,
   store,
-=======
->>>>>>> 195dafdae9287fdcc86cea3f94a2cb102b744cd8
   render: h => h(App)
 }).$mount("#app");
