46efdac847dbc4b386c48e26131568a37b1a6bc9
박수아 <xlalfskdl@naver.com>
2021-03-25 15:35:33 +0900

    [ADD] feature get car images for camera and gallery
    Former-commit-id: 234ea2098297bfee23b2febb6468faf67619e633 [formerly 0418c97eb97a09a52c3f511303d16becf9ad3e71] [formerly 4a872c718e75e6fa6a8d25719fcb69ca32d11f6e [formerly 3b9e32732f82396c0a67f307c30adb2997542349]]
    Former-commit-id: 0f3ecf6e45d90fe0f3688b25b8c35392be24102a [formerly a52f462a5f80718fe1a1d08a1a86e7fb9ff487b9]
    Former-commit-id: 287b43c77a51b777acf21e56bb0d1ddf29d77ed2

--- frontend/src/views/carSearch/CarSearch.vue
+++ frontend/src/views/carSearch/CarSearch.vue
@@ -3,16 +3,21 @@
     <h2>
       눈 앞의 차를 카메라로 찍어보고, 시세를 확인해보세요.
     </h2>
-    <CarGellery/>
+    <CarImages 
+    imageMethod="camera"
+    />
+    <CarImages
+    imageMethod="gallery"
+    />
   </div>
 
 </template>
 <script lang="ts">
-import CarGellery from '@/components/carSearch/CarGellery.vue'
+import CarImages from '@/components/carSearch/CarImages.vue'
 import Vue from 'vue'
 export default Vue.extend({
   components: { 
-    CarGellery,
+    CarImages,
   },
   
 })
