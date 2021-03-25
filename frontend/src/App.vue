1a5d60c4506720d7a35564e14e8643aaa40afc6a
박수아 <xlalfskdl@naver.com>
2021-03-25 14:29:35 +0900

    [ADD] skeleton main page
    Former-commit-id: 19b5f2f75ddd5a8745563fde107069db5f79baa1 [formerly b526bf6d3ad33b4ed8c1c864aa4c39c9617710af] [formerly 1558d2b5ba7bd2e705b51b2957ed9d36fa1bc386 [formerly 443b428713a287fd79778d14fb5278599baae4e8]]
    Former-commit-id: af95dc57521aaa4f2b399971690e0929c8005445 [formerly b67f49f5c103c6478795179fdc824e45c7f6f97f]
    Former-commit-id: a463506b1eb35ced5911c0a1160106920f541ca3

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,12 @@
 <template>
   <div id="app">
-    <CarNews/>
     <LoginPage/>
-    <div>사진찍기</div>
     <CarSearch/>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import CarNews from './components/carInfo/CarNews.vue';
 import CarSearch from './views/carSearch/CarSearch.vue';
 import LoginPage from './views/user/LoginPage.vue';
 
@@ -18,7 +15,6 @@ export default Vue.extend({
   components: { 
     LoginPage, 
     CarSearch,
-    CarNews 
     },
 
 });
