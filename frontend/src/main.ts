092ee292f7653349aa40ee6cca59b5daab963682
박수아 <xlalfskdl@naver.com>
2021-03-26 13:23:02 +0900

    [ADD] alert lib and example
    Former-commit-id: de602ad7c8cf161506204770ce8b6b1c7ac30332 [formerly 9d8afbb3134e1ef0d2a07ded00745b8c7e750202] [formerly 3f21bb4a39d3bee0cc4353176e7b79c536d75e6b [formerly d74fc2a82c75cd5c3b1577737aa34cfe42a8104b]]
    Former-commit-id: 1e6f38665191bf66484c42434fe16beabe584830 [formerly c5d353520b28278e72def5c4d5c7a43f5e43e15b]
    Former-commit-id: 6bb488537069963fd3ff6a98284b17743c235efb

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,6 +6,11 @@ import '@/css/index.css'
 import GAuth from 'vue-google-oauth2'
 // import VueWebCam from "vue-web-cam";
 
+import VueSweetalert2 from "vue-sweetalert2";
+import 'sweetalert2/dist/sweetalert2.min.css';
+
+Vue.use(VueSweetalert2);
+
 Vue.use(GAuth, {clientId: '239626612902-num4kon1amg6h3bkgart12e1q18hqprq.apps.googleusercontent.com', scope: 'profile email'})
 
 // Vue.use(VueWebCam)
