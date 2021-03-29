5a476631389e6a8dd779a121900d02689778888a
김채린 <kimchaelin13@gmail.com>
2021-03-29 14:59:05 +0900

    [ADD]뉴스 구조 수정
    Former-commit-id: bd904d5d089a44b8bfdd35c72cdc58adfde2b045 [formerly e385d24b179b201fd301fc29d602c0248fe9d647] [formerly fca51c2cb1c99adfb227803b9e2cb942803a11b3 [formerly e05fe2a61b1ba7693101498c099fc0763b67019d]]
    Former-commit-id: e2e5cbd61cd81d12307156c8b15f0ecbdb2cb324 [formerly b8f7ee319893fb067cf974b52f6fed9390793d3e]
    Former-commit-id: 29ee6932c47ac3a7eb0ae54a09cc4be7ca1c4ce3

--- frontend/src/components/carInfo/CarNews.vue
+++ frontend/src/components/carInfo/CarNews.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-    CarNews
+    <h1>뉴스</h1>
+
+    <h1>리뷰</h1>
+
+    <h1>모델 비교</h1>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-export default Vue.extend({
-  
-})
+import Vue from "vue";
+export default Vue.extend({});
 </script>
 
-<style>
-
-</style>
+<style></style>
