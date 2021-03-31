d1951e224b4f2afe6e6cc079300e32a9580bd1ab
김채린 <kimchaelin13@gmail.com>
2021-03-31 15:23:38 +0900

    [ADD]메인페이지-뉴스
    Former-commit-id: e2ee13a50e818ccd4661ce5490afa74de7c52256 [formerly 5e13fc1d76a7ba47763407970ea923a23922f51e] [formerly 6c609d11f0e5099269897508eca786aa5f8464c4 [formerly e8a6ae31db293c751187b2f885b8d174e6f98269]]
    Former-commit-id: 03232f8699e01f6d72b01d493948a7d5e116cf05 [formerly 256cde2433fd557815e9cad783db1119e8d4d972]
    Former-commit-id: d6109c9041abc53eb8d89eecfd77fb02059951b7

--- frontend/src/components/carInfo/CarNewsItem.vue
+++ frontend/src/components/carInfo/CarNewsItem.vue
@@ -1,16 +1,26 @@
 <template>
-  <div class="news">
-    {{ news }}
+  <div class="newsItem">
+    <div class="newsItem__title">{{ news.title }}</div>
+    <div class="newsItem__desc">{{ news.description }}</div>
+    <a :href="this.news.link" class="newsItem__more">원문보기</a>
   </div>
 </template>
 
 <script lang="ts">
+import { createLogger } from "node_modules/vuex/types";
 import Vue from "vue";
 export default Vue.extend({
   props: {
     news: {
       type: Object
     }
+  },
+  methods: {
+    goNewsLink() {
+    //   const newsLink = this.news.link;
+
+
+    // }
   }
 });
 </script>
