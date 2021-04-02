bfecbb47f84680a3617f36249f3154f78f15d637
박수아 <xlalfskdl@naver.com>
2021-04-02 16:54:55 +0900

    [ADD] modify car image search
    Former-commit-id: 38eb8c423cd6b01bb2b2a67d8b7d0b345dfe3b1a [formerly f6a1a266d02645fdfd3c79c552718e041645a53c] [formerly ac48ced1887de30a4ebbc89b52dd50f7b50282f8 [formerly 36323b52c03c7935864cafbfa40f5d0a0fada3d1]]
    Former-commit-id: 9cc2504bb2a79d941a3c598505d9f4977ade04f3 [formerly 26c1fc85d3ce59cb5405a2913270327024b62d7e]
    Former-commit-id: f43fbb2d1e02e255345f2230c53835871150cfe6

--- frontend/src/components/carSearch/CarImages.vue
+++ frontend/src/components/carSearch/CarImages.vue
@@ -1,105 +1,90 @@
 <template>
+  <div>
     <div>
-      <div>
-        <input 
-        ref="imageInput" 
-        type="file" 
-        hidden 
-        @change="onChangeImages" 
-        :v-model="carImagePic" 
+      <input
+        ref="imageInput"
+        type="file"
+        hidden
+        @change="onChangeImages"
+        :v-model="carImagePic"
         accept="image/jpeg,image/jpg"
-        >
-        <!-- :capture="imageMethod" -->
-        <button 
-        type="button"
-        @click="onClickImageUpload"
-        class="imageupload"
-        >
-          <template v-if="imageMethod==='camera'">
-            <i class="fas fa-camera-retro"></i>
-          </template>
-          <template v-else>
-            <i class="fas fa-image"></i>
-          </template>
-          {{ imageMethod }}
-        </button>
-        <CarImageBtn 
-        text="Upload" 
-        ref="zzUpload" 
-        @progress="moveProgress" @progress-finished="endProgress"/>
-
+      />
+      <button type="button" @click="onClickImageUpload" class="imageupload">
+        <i class="fas fa-camera-retro"></i>
+        Camera
+      </button>
+      <div @click="onClickImageUpload">
+        <CarImageBtn
+          text="Camera"
+          ref="zzUpload"
+          @progress="moveProgress"
+          @progress-finished="endProgress"
+        />
       </div>
-    <img 
-    v-if="imageUrl"
-    :src="imageUrl"
-    width="100%" 
-    alt="이미지">
+    </div>
+    <img v-if="imageUrl" :src="imageUrl" width="100%" alt="이미지" />
   </div>
-    
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import CarImageBtn from '@/components/carSearch/CarImageBtn.vue'
+import Vue from "vue";
+import CarImageBtn from "@/components/carSearch/CarImageBtn.vue";
+import { searchModelImg } from "@/api/index";
 
 export default Vue.extend({
-  components:{
+  components: {
     CarImageBtn
   },
-  props :{
-    imageMethod :{
-      type : String,
-      required : true,
-      default:"camera"
-    }
-  },
+
   data() {
     return {
       imageUrl: null,
-      carImagePic:String,
-    }
+      carImagePic: String
+    };
   },
   methods: {
     onClickImageUpload() {
-    this.$refs.imageInput.click();
+      this.$refs.imageInput.click();
     },
     onChangeImages(e) {
-        console.log(e.target.files)
-        const file = e.target.files[0]; // Get first index in files
-        this.imageUrl = URL.createObjectURL(file); // Create File URL
-        console.log('이미지 URL : ',this.imageUrl)
-        // this.sendImages();
+      console.log(e.target.files);
+      const file = e.target.files[0]; // Get first index in files
+      this.imageUrl = URL.createObjectURL(file); // Create File URL
+      console.log("이미지 URL : ", this.imageUrl);
+      this.sendImages();
     },
-    // sendImages() {
-    //   if(this.imageUrl) {
-    //       let imagePic = new FormData();
-    //       let pic = "보낼 이미지 이름";
-    //       let picDB = String(pic + '.jpg');
-    //       this.carImagePic = picDB;
-    //       imagePic.append("profile", this.imageUrl, String(pic+'.jpg'))
-    //       await API보내기(imagePic)
-    //       .then(() => {
-    //       })
-    //       .catch(() => {
-    //         this.$swal({
-    //           text: "프로필 사진을 업로드 하던 중 오류가 발생했습니다.",
-    //           customClass: {
-    //             container: 'swal2-container'
-    //           },
-    //           icon: 'warning',
-    //           timer: 1300,
-    //           showConfirmButton: false,
-    //         })
-    //         return false;
-    //       });
-    //     }
-//     }
+    sendImages() {
+      if (this.imageUrl) {
+        this.$refs.zzUpload.progressClickEvent();
+        const imagePic = new FormData();
+        const pic = "보낼 이미지 이름";
+        const picDB = String(pic + ".jpg");
+        this.carImagePic = picDB;
+        imagePic.append("profile", this.imageUrl, String(pic + ".jpg"));
+        searchModelImg(imagePic)
+          .then(res => {
+            console.log(res);
+          })
+          .catch(() => {
+            this.$swal({
+              text: "사진을 업로드 하던 중 오류가 발생했습니다.",
+              customClass: {
+                container: "swal2-container"
+              },
+              icon: "warning",
+              timer: 1300,
+              showConfirmButton: false
+            });
+            return false;
+          });
+      }
+    },
     moveProgress(progress = 0) {
-      console.log('3 moveProgress시작')
+      console.log("3 moveProgress시작", progress);
       // 업로드 api로 바꾸기 그만큼 시간흐름
       const progressTimeout = setTimeout(() => {
         clearTimeout(progressTimeout);
-        
+
         if (progress < 100) {
           const newProgress = progress + 1;
           this.$refs.zzUpload.moveProgress(newProgress);
@@ -110,12 +95,11 @@ export default Vue.extend({
     endProgress() {
       const endProgressTimeout = setTimeout(() => {
         clearTimeout(endProgressTimeout);
-        
+
         this.$refs.zzUpload.resetProgress();
-      }, 5000);
+      }, 1000);
+      this.$router.push({ name: "Main" });
     }
   }
-
-    
-})
+});
 </script>
