653bcf21ab5f9008eb1bcddb8f7b6d03855339d6
박수아 <xlalfskdl@naver.com>
2021-04-02 17:42:36 +0900

    [ADD] add props and api in Mainpage
    Former-commit-id: 4046c85453601675e0419d3b80010558f8ed0511 [formerly 0383c676c18887f749591d2b332569755c512723] [formerly d19a8aaa2fcc783fd72e58f125c82ab866750e73 [formerly d3c097d0bb280f3d82d956d6bd544628c6c2f56b]]
    Former-commit-id: 945f84bb1f3fbe8e249fa4f7b2b895decbada6d7 [formerly 49866823535bb3fc5370ecc5d2fd66bdc4503831]
    Former-commit-id: 2dbd2e66bb06e52beaa96710d204f6fba8a3b4b8

--- frontend/src/views/main/MainPage.vue
+++ frontend/src/views/main/MainPage.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
     <!-- 차량 분석 컴포넌트 -->
-    <CarSearchResult />
+    <CarSearchResult :modelInfo="modelInfo" />
     <!-- 차량 시세 차트 -->
-    <CarPrice />
+    <CarPrice :allOptions="allOptions" />
     <!-- 차량 비교 차트 -->
-    <CarCompare />
+    <CarCompare :latestModel="latestModel" :modelInfo="modelInfo" />
     <!-- 차량 모델비교(가격) -->
     <CarModel :CompareCar="similarPriceCar" />
     <!-- 차량 모델비교(차체크기) -->
     <CarModel :CompareCar="sameSegmentCar" />
     <!-- 차량 뉴스 -->
-    <CarNews />
+    <CarNews :carNews="carNews" />
     <!-- 차량 댓글 비교(텍스트마이닝 된다면) -->
     <!-- <CarReview/> -->
     <!-- 차량 굿즈 -->
-    <CarGoods />
+    <CarGoods :carGoods="carGoods" />
   </div>
 </template>
 
@@ -27,11 +27,23 @@ import CarGoods from "@/components/carInfo/CarGoods.vue";
 import CarPrice from "@/components/carInfo/CarPrice.vue";
 import CarModel from "@/components/carInfo/CarModel.vue";
 import CarCompare from "@/components/carInfo/CarCompare.vue";
-import { fetchPriceCompare, fetchSizeCompare } from "@/api/index";
+import {
+  fetchModel,
+  fetchAllOptions,
+  fetchLatest,
+  fetchPriceCompare,
+  fetchSizeCompare,
+  fetchNews,
+  fetchShops
+} from "@/api/index";
 
 export default Vue.extend({
   props: {
     m_id: {
+      type: Number,
+      default: 0
+    },
+    md_id: {
       type: Number,
       default: 0
     },
@@ -42,8 +54,13 @@ export default Vue.extend({
   },
   data() {
     return {
-      similarPriceCar: {},
-      sameSegmentCar: {}
+      modelInfo: Object,
+      allOptions: Array,
+      latestModel: Object,
+      similarPriceCar: Object,
+      sameSegmentCar: Object,
+      carNews: Array,
+      carGoods: Array
     };
   },
   components: {
@@ -55,17 +72,42 @@ export default Vue.extend({
     CarCompare
   },
   created() {
+    this.getModelInfo();
+    this.getCarAllOptions();
+    this.getLatestModel();
     this.getSimilarPriceModel();
     this.getSameSegmentModel();
+    this.getCarNews();
+    this.getCarGoods();
   },
   methods: {
+    async getModelInfo() {
+      const { data } = await fetchModel(this.m_id);
+      this.modelInfo = data;
+    },
+    async getCarAllOptions() {
+      const { data } = await fetchAllOptions(this.m_id);
+      this.allOptions = data;
+    },
+    async getLatestModel() {
+      const { data } = await fetchLatest(this.m_id);
+      this.latestModel = data;
+    },
     async getSimilarPriceModel() {
-      const response = await fetchPriceCompare(this.m_id);
-      this.similarPriceCar = response.data;
+      const { data } = await fetchPriceCompare(this.m_id);
+      this.similarPriceCar = data;
     },
     async getSameSegmentModel() {
-      const response = await fetchSizeCompare(this.m_id);
-      this.sameSegmentCar = response.data;
+      const { data } = await fetchSizeCompare(this.m_id);
+      this.sameSegmentCar = data;
+    },
+    async getCarNews() {
+      const { data } = await fetchNews(this.m_name);
+      this.carNews = data;
+    },
+    async getCarGoods() {
+      const { data } = await fetchShops(this.m_name);
+      this.carGoods = data;
     }
   }
 });
