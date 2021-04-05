596dec65cebad725c9765e112e1b02290c6d930e
박수아 <xlalfskdl@naver.com>
2021-04-05 16:37:52 +0900

    [ADD] aboutPage skeleton
    Former-commit-id: 85b3725058edf028cc59d64d7512932d09582a26 [formerly b7bd63325679eb08e09aee1a37ade9af2c4d0e01] [formerly 1786d8362f500e6f3dec8fc3a2ca6afef0a81079 [formerly 9ab2980ca33c7ef30949487cac2e6821ff5372fb]]
    Former-commit-id: 3d62109977dddf079df005ef8c85aa1e720538d9 [formerly dea28d9ddf763fe33eb847be7f7839099036fa9e]
    Former-commit-id: 41276ffb9353701c540a0016391a81953e326c18

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <Nav />
-    <h1 class="LOGO" @click="goImgSearch">KOMENCAR</h1>
+    <Nav v-if="isShow" />
+    <h1 v-if="isShow" class="LOGO" @click="goImgSearch">KOMENCAR</h1>
     <!-- <div class="Logo__sign">
       <span class="Logo__fast-flicker">KO</span>MEN<span class="Logo__flicker"
         >C</span
@@ -20,6 +20,20 @@ import Nav from "@/components/common/Nav.vue";
 export default Vue.extend({
   components: { Nav },
   name: "App",
+  data() {
+    return {
+      isShow: true as boolean
+    };
+  },
+  watch: {
+    $route() {
+      if (this.$route.name !== "AboutPage") {
+        this.isShow = true;
+      } else {
+        this.isShow = false;
+      }
+    }
+  },
   methods: {
     goImgSearch() {
       this.$router.push({ name: "CarSearch" });
---
+++ frontend/src/views/about/AboutPage.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="wrap-3d">
+    <div class="item-3d">
+      <span class="ground"></span>
+      <figure class="item-content">
+        <figcaption class="item-caption">
+          <p>
+            내 손안의<strong> AI KOMENCAR</strong>가 알려주는
+            <strong> 가격정보</strong><br />
+            눈 앞에 있는 차를
+            <strong>카메라로</strong> 찍고,<br />
+          </p>
+          <p>
+            <strong>가격까지</strong>
+            한번에 확인할 수 있어요
+          </p>
+          <p>
+            <!-- <a href="#">View 38mm in the store</a><br />
+            <a href="#">View 42mm in the store</a> -->
+          </p>
+        </figcaption>
+        <div class="item-img">
+          <img src="../../assets/images/the_new_grandeur.png" alt="" />
+        </div>
+      </figure>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({});
+</script>
+
+<style></style>
