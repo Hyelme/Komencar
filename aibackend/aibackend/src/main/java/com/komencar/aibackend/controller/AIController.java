c02f8ea129982abfc1747100355b382169704367
배상웅 <dentist312@naver.com>
2021-04-05 19:36:52 +0900

    Connect Swagger
    Former-commit-id: 6124033f909ab14adf50ef31dc34922efa614515 [formerly 0180c580dd7a01458169baad24d50cec205b97a1] [formerly 69de8f32e355d4f495e20bc19d9aa539b594d7ac [formerly 7062134a606c7c3878b28cf30a22246ef5601629]]
    Former-commit-id: cff5f318717d170d6c553f8bff5de40200f6c4ea [formerly 319e3866ec0829ea3ab776b57d923c3170d73f59]
    Former-commit-id: a68495c3ec3cefa6c81fea8661bfd3d8b4657971

--- aibackend/aibackend/src/main/java/com/komencar/aibackend/controller/AIController.java
+++ aibackend/aibackend/src/main/java/com/komencar/aibackend/controller/AIController.java
@@ -37,7 +37,7 @@ public class AIController {
             @ApiImplicitParam(name = "posterfile", value = "jpg file", dataType = "file", required = true),
     })
     @PostMapping("/picture")
-    public String predictPicture(@RequestParam("file") MultipartFile file, HttpServletRequest request) {
+    public String predictPicture(@RequestBody MultipartFile file, HttpServletRequest request) {
 
         String pic_path = "test_images/"+file_name+"_"+file.getOriginalFilename();
         File targetFile = new File(pic_path);
