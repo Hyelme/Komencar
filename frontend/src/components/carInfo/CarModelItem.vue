822ab1daddd0319590b3224793dfe74eabe6555a
김채린 <kimchaelin13@gmail.com>
2021-04-05 16:40:04 +0900

    [ADD]모델 가격비교 수정 CSS
    Former-commit-id: 24be982d413b87158b0e675d249b92758748bf3e [formerly 4b87252dd9e5e455c81cff025475b95e8f435cce] [formerly 84fc2c2bf9fc0e8b6d3a53606225094255572e62 [formerly 1b9634b2ec4f01b9b4b8a0518dfdd1e36e25eda6]]
    Former-commit-id: 77806be2aba69257c220c4dda0fda2da64f3fd4a [formerly b8a45bb9eae5a87204be1570f98c59cc67a8aad5]
    Former-commit-id: 329ab745ce9f1ddd5018e74083a2225860187b23

--- frontend/src/components/carInfo/CarModelItem.vue
+++ frontend/src/components/carInfo/CarModelItem.vue
@@ -1,13 +1,19 @@
 <template>
-    <div>
-        {{ compareCar.m_name }}
-        
-        <img :src="this.compareCar.m_image" alt="비교분석이미지" width="150px">
-        {{ compareCar.m_price }}
-        {{ compareCar.m_effciency }}
-
-
+  <div class="item-box">
+    <div class="img-wrap">
+      <img :src="this.compareCar.m_image" alt="" />
     </div>
+    <div class="caption">
+      <div class="caption__desc">
+        <div class="caption__desc__title">{{ compareCar.m_name }}</div>
+        <div class="caption__desc__detail">
+          가격 : {{ compareCar.m_price }}<br />연비 :
+          {{ compareCar.m_effciency }}
+        </div>
+      </div>
+      <a class="caption__desc__button" href="#">See details</a>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
