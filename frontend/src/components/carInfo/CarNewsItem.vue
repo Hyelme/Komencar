fe5a096e437b29fb09cf6fce53e2dddba6fdd121
Hyerim Jeong <hrjeong8@gmail.com>
2021-04-06 15:27:39 +0900

    [UPDATE] update car news page css
    Former-commit-id: e605336366dce235eb1b779b96b20464fa901cc6 [formerly 29ea25a3edb90a263814d3c7ed9558f3129721eb] [formerly 04d875ab13d7b43f1c291ec654cd04f72c5b09dc [formerly bec2b8ce80f1fd041963b6c91a3916e8336d8726]]
    Former-commit-id: b1eb4a5aa2db20a91a840dbb5da965f1b8e05300 [formerly a29e9fbefb14d5f27eb41255464dba11af603467]
    Former-commit-id: bf8dacbefdb4f60ade01bb40375083e6ad4b0144

--- frontend/src/components/carInfo/CarNewsItem.vue
+++ frontend/src/components/carInfo/CarNewsItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="newsItem">
-    <div class="newsItem__title">{{ news.title }}</div>
-    <div class="newsItem__desc">{{ news.description }}</div>
+    <div class="newsItem__title" v-html="news.title"></div>
+    <div class="newsItem__desc" v-html="description"></div> <!-- {{ news.description }} -->
     <a :href="this.news.link" class="newsItem__more">원문보기</a>
   </div>
 </template>
@@ -14,13 +14,51 @@ export default Vue.extend({
     news: {
       type: Object
     }
-  }
-  // methods: {
+  },
+  data() {
+    return {
+      title: String,
+      description: String,
+      width: 0,
+    }
+  },
+  created() {
+    this.handleResize();
+  },
+  mounted() {
+    window.addEventListener('resize', this.handleResize);
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.handleResize);
+  },
+  methods: {
   //   goNewsLink() {
   //   //   const newsLink = this.news.link;
 
   //   // }
-  // }
+    // setDescription(){
+    //   this.description = this.news.description;
+
+    //   if(this.description.length >  )
+    // }
+    handleResize(event) {
+        this.width = window.innerWidth;
+        console.log("this is news : ",this.news);
+        if(this.width < 300){
+          this.description = this.news.description.substr(0,30)+"...";
+        }
+        else if(this.width > 300 && this.width < 600){
+          this.description = this.news.description.substr(0,66)+"...";
+        } //모바일 기기
+        else if(this.width > 600 && this.width < 1080) {
+          this.title = this.news.title;
+          this.description = this.news.description.substr(0,120)+"...";
+        } //태블릿 기기
+        else if(this.width > 1080) {
+          this.description = this.news.description;
+        } //웹 화면
+    }
+  }
 });
 </script>
 
