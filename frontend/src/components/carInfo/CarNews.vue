3d645921975345790f6b87bd23da9e1894075aed
박수아 <xlalfskdl@naver.com>
2021-04-06 16:45:48 +0900

    [DEL]del  carnews methods
    Former-commit-id: 086da7907a48b0401c393fb8e604dea1b015f2e6 [formerly 8a9ec5c45dbdca8a1028bf2494819792eb1376f5] [formerly 17e1422f0b17cd1c349879255d4d8a5360b32321 [formerly 9e789dffa991259aee00c47a0bfebce432e45ae4]]
    Former-commit-id: fd49a6a98e5ca03489db1d6cc2dacdb0b0b19a3c [formerly c16333e2dc7d45313ecec36e4366d0a13a0b13c9]
    Former-commit-id: 5da324c1149a99db10df21978f935fd2ab463351

--- frontend/src/components/carInfo/CarNews.vue
+++ frontend/src/components/carInfo/CarNews.vue
@@ -27,31 +27,23 @@ export default Vue.extend({
   data() {
     return {
       // newsData: [
-        //   {
-          //     title: "현대차 7세대 그랜저, 2022년 대형차급으로 복귀" as string,
+      //   {
+      //     title: "현대차 7세대 그랜저, 2022년 대형차급으로 복귀" as string,
       //     originallink: "https://www.hankyung.com/car/article/2021032596012" as string,
       //     link: "https://www.hankyung.com/car/article/2021032596012" as string,
       //     description: "현대자동차가 그랜저의 지위를 준대형에서 대형 세단으로 격상할 전망이다. 1세대 '각 그랜저' 시절 성공의 상징으로 불렸던 플래그십 세단의 옛명성을 되찾는다는 계획이다." as string,
       //     pubDate: "2021.03.25 08:01" as string
       //   },
       //   {
-        //     title: "현대차 7세대 그랜저, 2022년 대형차급으로 복귀" as string,
+      //     title: "현대차 7세대 그랜저, 2022년 대형차급으로 복귀" as string,
       //     originallink: "https://www.hankyung.com/car/article/2021032596012" as string,
       //     link: "https://www.hankyung.com/car/article/2021032596012" as string,
       //     description: "현대자동차가 그랜저의 지위를 준대형에서 대형 세단으로 격상할 전망이다. 1세대 '각 그랜저' 시절 성공의 상징으로 불렸던 플래그십 세단의 옛명성을 되찾는다는 계획이다." as string,
       //     pubDate: "2021.03.25 08:01" as string
       //   }
       // ]
-      newsList:Array,
+      newsList: Array
     };
-  },
-  created() {
-    this.defaultNews();
-  },
-  methods: {
-    defaultNews(){
-
-    }
-  },
+  }
 });
 </script>
