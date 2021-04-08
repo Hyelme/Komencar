183ce0427fa332d219e096859788c8fd30a8ad0c
Hyerim Jeong <hrjeong8@gmail.com>
2021-04-08 14:23:51 +0900

    [FIX] fix car goods slider
    Former-commit-id: 4c88f5eb6128edfad0288af7358f2374fb50c6c3 [formerly d73a46d2e22c21ff04160b107782f65688349088] [formerly 62cf5cc92090e944feb9552ab85133a6c4686d81 [formerly 002b74eb0da63b7fac8a4489579e17d723a2698c]]
    Former-commit-id: 2b94c0cdd4256620b2757c6f20bdd341b98ecce2 [formerly 199e6a854d7fd03cac160b08fd2bccb6be323422]
    Former-commit-id: 5eca60e6664a62b36994707c6c993d9b5bf9a595

--- frontend/src/components/carInfo/CarGoods.vue
+++ frontend/src/components/carInfo/CarGoods.vue
@@ -4,9 +4,19 @@
     <div class="box__subtitle">
       {{ CarModelName }} 에 어울리는 굿즈를 쇼핑해보세요
     </div>
-    <div v-for="(goods, index) in goodsData" :key="index" class="box__goodsBox">
-      <CarGoodsItem :goods="goods" />
+
+    <div v-swiper:mySwiper="swiperOption" class="swiper-container">
+      <div class="swiper-wrapper">
+        <div v-for="(goods, index) in goodsData" :key="index" class="swiper-slide box__goodsBox">
+          <CarGoodsItem :goods="goods" />
+        </div>
+      </div>
+      <div class="swiper-pagination" v-if="slides.length > 1"></div>
     </div>
+
+    <!-- <div v-for="(goods, index) in goodsData" :key="index" class="box__goodsBox">
+      <CarGoodsItem :goods="goods" />
+    </div> -->
   </div>
 </template>
 
@@ -16,7 +26,9 @@ import Vue from "vue";
 import CarGoodsItem from "@/components/carInfo/CarGoodsItem.vue";
 
 export default Vue.extend({
-  components: { CarGoodsItem },
+  components: { 
+    CarGoodsItem,
+  },
   props: {
     goodsData: {
       type: Array
@@ -24,36 +36,39 @@ export default Vue.extend({
     CarModelName: {
       type: String
     }
-  }
-  // data() {
-  //   return {
-  //   goodsData: [
-  //     {
-  //       title: "메이튼 자동차 코일매트 확장형" as string,
-  //       link: "https://search.shopping.naver.com/catalog/17389272175?query=%EC%95%84%EB%B0%98%EB%96%BC&NaPm=ct%3Dkmvzg72w%7Cci%3D8a2bc10ffcfc486d4865cba0542ec26deadc261f%7Ctr%3Dslsl%7Csn%3D95694%7Chk%3D42539be5b4c0d4fba2d005e7eb15c3c6fdf5d005" as string,
-  //       image: "https://shopping-phinf.pstatic.net/main_1738927/17389272175.3.20200922133836.jpg?type=f640" as string,
-  //       lprice: "23,300" as string,
-  //       hprice: "37,900" as string,
-  //       mallName: "메이튼" as string
-  //     },
-  //     {
-  //       title: "메이튼 자동차 코일매트 확장형" as string,
-  //       link: "https://search.shopping.naver.com/catalog/17389272175?query=%EC%95%84%EB%B0%98%EB%96%BC&NaPm=ct%3Dkmvzg72w%7Cci%3D8a2bc10ffcfc486d4865cba0542ec26deadc261f%7Ctr%3Dslsl%7Csn%3D95694%7Chk%3D42539be5b4c0d4fba2d005e7eb15c3c6fdf5d005" as string,
-  //       image: "https://shopping-phinf.pstatic.net/main_1738927/17389272175.3.20200922133836.jpg?type=f640" as string,
-  //       lprice: "23,300" as string,
-  //       hprice: "37,900" as string,
-  //       mallName: "메이튼" as string
-  //     },
-  //     {
-  //       title: "메이튼 자동차 코일매트 확장형" as string,
-  //       link: "https://search.shopping.naver.com/catalog/17389272175?query=%EC%95%84%EB%B0%98%EB%96%BC&NaPm=ct%3Dkmvzg72w%7Cci%3D8a2bc10ffcfc486d4865cba0542ec26deadc261f%7Ctr%3Dslsl%7Csn%3D95694%7Chk%3D42539be5b4c0d4fba2d005e7eb15c3c6fdf5d005" as string,
-  //       image: "https://shopping-phinf.pstatic.net/main_1738927/17389272175.3.20200922133836.jpg?type=f640" as string,
-  //       lprice: "23,300" as string,
-  //       hprice: "37,900" as string,
-  //       mallName: "메이튼" as string
-  //     }
-  //   ]
-  // };
-  // },
+  },
+  data() {
+    return {
+      slides: '',
+        swiperOption: {
+          loop: false,
+          slidesPerView: 2,
+          spaceBetween: 30,
+          pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            dynamicBullets: true,
+            dynamicMainBullets : 2
+          },
+          //반응형
+          breakpoints: {
+            // when window width is >= 480px
+            480: {
+              slidesPerView: 2,
+              spaceBetween: 10
+            },
+            // when window width is >= 640px
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 20
+            },
+            1264: {
+              slidesPerView: 3,
+              spaceBetween: 27
+            }
+          }
+        },
+    };
+  },
 });
 </script>
